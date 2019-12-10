e0aaccf366c0b7f7ada72d7b68d31b0fe33018c4
igor.js <igor.js@hitech18.online>
2019-12-10 15:49:38 +0200

    Ready tls connect with auth.

--- client.js
+++ client.js
@@ -2,20 +2,33 @@
 const tls = require('tls');
 const fs = require('fs');
 
-const options = {
-	ca: [ fs.readFileSync(`${__dirname}/tsl/server-cert.pem`) ]
-};
-
 const port = 8000;
+/** Note: With example use only server cert */
+const options = {
+
+	// Necessary only if the server requires client certificate authentication.
+	key: fs.readFileSync(`${__dirname}/tsl/server-key.pem`),
+	cert: fs.readFileSync(`${__dirname}/tsl/server-cert.pem`),
+
+	// Necessary only if the server uses a self-signed certificate.
+	ca: [ fs.readFileSync(`${__dirname}/tsl/server-cert.pem`) ],
+
+	// Necessary only if the server's cert isn't for "localhost".
+	checkServerIdentity: () => { return null; },
+};
 const host = 'localhost';
 
 const socket = tls.connect(port, host, options, () => {
 	console.log('client connected', socket.authorized ? 'authorized' : 'unauthorized');
+	console.log('authorizationError ', socket.authorizationError);
+
 	process.stdin.pipe(socket);
 	process.stdin.resume();
 });
 
 socket.setEncoding('utf8');
+
+socket.on('error', console.error)
 
 socket.on('data', (data) => {
 	console.log(data);
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const tls = require('tls');
-const fs = require('fs');
+const fs  = require('fs');
 
 const port  = 8000;
+/** Note: With example use only server cert */
 const options = {
 	key: fs.readFileSync(`${__dirname}/tsl/server-key.pem`),
 	cert: fs.readFileSync(`${__dirname}/tsl/server-cert.pem`),
 
-	rejectUnauthorized: true,
+	// This is necessary only if using client certificate authentication.
+	requestCert: true,
+	// rejectUnauthorized: false,
+
+	//client
+	ca: [ fs.readFileSync(`${__dirname}/tsl/server-cert.pem`) ]
 };
 
 const server = tls.createServer(options, (socket) => {
-	console.log('server connected',
-		socket.authorized ? 'authorized' : 'unauthorized');
+	console.log('server connected', socket.authorized ? 'authorized' : 'unauthorized');
+
+	console.log('authorizationError ', socket.authorizationError);
+	console.log('getProtocol()  ', socket.getProtocol());
+
 	socket.write('welcome!\n');
 	socket.setEncoding('utf8');
 	socket.pipe(socket);
